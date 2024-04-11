7c40e6823917a36ae0c96e8663e35cc3fac2f4fd
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-04-11 19:27:15 +0200

    remove try catch

--- src/utils/registrySubscription.js
+++ src/utils/registrySubscription.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
     subscribeToApiGateway: async () => {
         // Order
-        try {
+        // try {
             const response = await axios({
                 method: "POST",
                 baseURL: `http://${process.env.GATEWAY_HOST}:${process.env.GATEWAY_PORT}`,
@@ -24,8 +24,8 @@ module.exports = {
                     ]
                 }
             });
-        } catch (error) {
-            console.log(error);
-        }
+        // } catch (error) {
+        //     console.log(error);
+        // }
     }
 }
